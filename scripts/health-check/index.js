c7f8dd7c17bcbe206212783fd466ea7e53ec2a4d
Pedro Gomes <pedrogomes94@gmail.com>
2021-09-01 14:07:54 +0200

    calculate average

--- scripts/health-check/index.js
+++ scripts/health-check/index.js
@@ -1,42 +1,75 @@
 const { checkHealth } = require("./healthcheck");
 
-const TIMEOUT = 5000;
+const TIMEOUT = 10000;
 
-defaultLogger = (message, objectStatus) => {
-  if (!process.env.STRESS_TEST) {
-    console.log(message, objectStatus);
-  }
+function calculateAverage(previousAverage, newValue, newTotal) {
+  const newAverage = previousAverage * ((newTotal - 1) / newTotal) + newValue * (1 / newTotal);
+  return Number(newAverage).toFixed(3);
+}
+
+const analysis = {
+  total: 0,
+  timeoutted: 0,
+  average: 0,
 };
 
-async function run(uri) {
-  if (!process.env.STRESS_TEST) {
-    console.log("Using bridge:", uri);
-  }
-  const result = await checkHealth(TIMEOUT, defaultLogger, uri);
-  if (result.alive) {
-    return `${uri} is alive, check took, ${result.durationSeconds} seconds`;
-  }
-  if (result.error) {
-    result.error.toString();
-    const msg = result.error.message;
-    return `⚠️ ⚠️ ${uri}: ${msg}⚠️ ⚠️ `;
+function addToAnalysis(params) {
+  analysis.total += 1;
+  if (params.timeoutted) {
+    analysis.timeoutted += 1;
+  } else if (params.elapsed) {
+    analysis.average = calculateAverage(
+      analysis.average,
+      params.elapsed,
+      analysis.total - analysis.timeoutted,
+    );
   }
 }
 
-tasks = [];
+function defaultLogger(message, objectStatus) {
+  if (!process.env.STRESS_TEST) {
+    console.log(message, objectStatus); // eslint-disable-line no-console
+  }
+}
+
+async function run(uri) {
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+  if (!process.env.STRESS_TEST) {
+    console.log("Using bridge:", uri); // eslint-disable-line no-console
+  }
+  const result = await checkHealth(TIMEOUT, defaultLogger, uri);
+  if (result.alive) {
+    addToAnalysis({ elapsed: result.durationSeconds });
+    return `${uri} is alive, check took, ${result.durationSeconds} seconds`;
+  }
+  if (result.error) {
+    addToAnalysis({ timeoutted: true });
+    result.error.toString();
+    const msg = result.error.message;
+    return `⚠️ ⚠️ ${uri}: ${msg} ⚠️ ⚠️`;
+  }
+}
+
+const promises = [];
 for (let i = 0; i < 1; i++) {
   let uri;
   try {
     uri = process.argv.slice(2)[0];
-  } catch {}
+  } catch {
+    // ignore error
+  }
 
   if (uri != undefined) {
-    tasks.push(run(uri));
+    promises.push(run(uri));
   } else {
-    const alpha = Array.from(Array(26)).map((e, i) => i + 97);
-    alphabet = alpha.map(x => String.fromCharCode(x));
-    alphabet.push(...["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-    alphabet.push(
+    const alphabet = Array.from(Array(26)).map((e, i) => i + 97);
+    const subdomains = alphabet.map(x => String.fromCharCode(x));
+    subdomains.push(...["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+    subdomains.push(
       ...[
         "uniswap",
         "ethermine",
@@ -65,14 +98,20 @@ for (let i = 0; i < 1; i++) {
       ],
     );
 
-    alphabet.forEach(url => {
-      tasks.push(run(`https://${url}.bridge.walletconnect.org`));
+    subdomains.forEach(url => {
+      promises.push(run(`https://${url}.bridge.walletconnect.org`));
     });
-    tasks.push(run(`https://bridge.walletconnect.org`));
+    promises.push(run(`https://bridge.walletconnect.org`));
   }
 }
 
-Promise.all(tasks).then(allResults => {
-  console.log(allResults);
+Promise.all(promises).then(results => {
+  // eslint-disable-next-line no-console
+  console.log(results);
+  // eslint-disable-next-line no-console
+  console.log(
+    `Tested ${analysis.total} servers where ${analysis.timeoutted} timeoutted and ${analysis.total -
+      analysis.timeoutted} succeeded with an average elapsed time of ${analysis.average} seconds`,
+  );
   process.exit(0);
 });
